a6efcbcbf50ba778d7d9326126861ab1c8993894
unknown <you@example.com>
2026-01-22 00:32:10 +0530

    fgtefcte

--- public/js/books.js
+++ public/js/books.js
@@ -7,13 +7,6 @@ const myBooksDatabase = [
         desc: "Shakespeare's timeless masterpiece.", 
         img: "https://images.unsplash.com/photo-1589998059171-988d887df646?w=400", 
         link: "vault/romeo_juliet.html" 
-    }, // <--- MUST have this comma
-    { 
-        category: "Fiction", 
-        title: "The Clockmaker's Silence", 
-        desc: "A mysterious tale of a man who controls time.", 
-        img: "https://images.unsplash.com/photo-1508057198894-247b23fe5ade?w=400", 
-        link: "vault/clockmaker.html" 
     } // <--- NO comma here if it is the last item
 ];
 
