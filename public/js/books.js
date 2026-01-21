2cef99641f2688c69d56b8f1d4dad801ece98da7
unknown <you@example.com>
2026-01-21 18:23:51 +0530

    fgtefcte

---
+++ public/js/books.js
@@ -0,0 +1,19 @@
+// js/books.js
+
+const myBooksDatabase = [
+    { 
+        category: "Novels", 
+        title: "The Tragedy of Romeo and Juliet", 
+        desc: "Shakespeare's timeless masterpiece.", 
+        img: "https://images.unsplash.com/photo-1589998059171-988d887df646?w=400", 
+        link: "vault/romeo_juliet.html" 
+    }, // <--- MUST have this comma
+    { 
+        category: "Fiction", 
+        title: "The Clockmaker's Silence", 
+        desc: "A mysterious tale of a man who controls time.", 
+        img: "https://images.unsplash.com/photo-1508057198894-247b23fe5ade?w=400", 
+        link: "vault/clockmaker.html" 
+    } // <--- NO comma here if it is the last item
+];
+
