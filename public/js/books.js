828a7029e161f14525e4dee4b7d11d371a51f6d2
unknown <you@example.com>
2026-01-24 16:08:12 +0530

    fgtefcte

--- public/js/books.js
+++ public/js/books.js
@@ -1,12 +1,23 @@
-// js/books.js
-
 const myBooksDatabase = [
-    { 
-        category: "Novels", 
-        title: "The Tragedy of Romeo and Juliet", 
-        desc: "Shakespeare's timeless masterpiece.", 
-        img: "https://images.unsplash.com/photo-1589998059171-988d887df646?w=400", 
-        link: "vault/romeo_juliet.html" 
-    } // <--- NO comma here if it is the last item
+    {
+        title: "The Tragedy of Romeo and Juliet",
+        categories: ["Novels", "fiction"], 
+        img: "https://images.unsplash.com/photo-1589998059171-988d887df646?w=400",
+        desc: "Shakespeare's timeless masterpiece.",
+        link: "vault/romeo_juliet.html"
+    },
+    {
+        title: "The Odyssey",
+        categories: ["Novels", "Classics", "fiction"],
+        img: "https://images.unsplash.com/photo-1580537659466-0a9bfa916a54?w=400",
+        desc: "Homer's epic tale of Odysseus.",
+        link: "vault/odyssey.html"
+    },
+    {
+        title: "The Adventures of Sherlock Holmes",
+        categories: ["Novels", "shortstories", "fiction"],
+        img: "https://images.unsplash.com/photo-1587876211306-694908bd42a7?w=400",
+        desc: "The world's greatest detective.",
+        link: "vault/sherlock_holmes.html"
+    }
 ];
-
