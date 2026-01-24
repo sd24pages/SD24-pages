84eaa2bfc6b13646a4cf868201fdbc0f6f589655
unknown <you@example.com>
2026-01-24 16:13:55 +0530

    fgtefcte

--- public/js/books.js
+++ public/js/books.js
@@ -8,14 +8,14 @@ const myBooksDatabase = [
     },
     {
         title: "The Odyssey",
-        categories: ["Novels", "Classics", "fiction"],
+        categories: ["Novels", "Fiction"],
         img: "https://images.unsplash.com/photo-1580537659466-0a9bfa916a54?w=400",
         desc: "Homer's epic tale of Odysseus.",
         link: "vault/odyssey.html"
     },
     {
         title: "The Adventures of Sherlock Holmes",
-        categories: ["Novels", "shortstories", "fiction"],
+        categories: ["Novels", "Short Stories", "Fiction"],
         img: "https://images.unsplash.com/photo-1587876211306-694908bd42a7?w=400",
         desc: "The world's greatest detective.",
         link: "vault/sherlock_holmes.html"
